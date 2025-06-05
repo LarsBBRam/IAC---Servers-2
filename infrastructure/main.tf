51f686002184ba0cb2dfc559725b99350e03b692
Lars Bendik Bjella Ramstad <brlb022025@gmail.com>
2025-06-05 10:27:12 +0200

    added output and resources

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,11 +1,28 @@
+resource "hcloud_ssh_key" "server_public_key" {
+  name = "server-ssh-key"  
+  public_key = file("./ed_id_admin.pub")
+
+}
+
 # Link to server resource docs
 # https://registry.terraform.io/providers/hetznercloud/hcloud/latest/docs/resources/server
 resource "hcloud_server" "our_server" {
   name = "our-server"
 
-  server_type = ""
-  image = ""
+  server_type = "cax21"
+  image = "debian-12"
 
-  ssh_keys = [ "" ]
+  ssh_keys = [ 
+    hcloud_ssh_key.server_public_key.id,
+  ]
 
+}
+
+output "server"{
+  description = "ip addresses to server"
+  value = {
+    ipv4 = hcloud_server.our_server.ipv4_address
+    ipv6 = hcloud_server.our_server.ipv6_address
+
+  }
 }
